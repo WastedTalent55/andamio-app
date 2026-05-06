4284f5ffcfbf81d92443088ce5e3401df1cfb553
Perro Maldito <88fritzz@gmail.com>
2026-05-05 22:26:23 -0600

    feat: maquetación inicial de tarjetas de clientes con estilos globales

--- andamio-frontend/src/app/app.routes.ts
+++ andamio-frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'clientes',
+    loadChildren: () => import('./modules/clientes/clientes.module').then(m => m.ClientesModule)
+  },
+  {
+    path: '',
+    redirectTo: 'clientes',
+    pathMatch: 'full'
+  }
+];
--- andamio-frontend/src/app/modules/clientes/clientes-routing.module.ts
+++ andamio-frontend/src/app/modules/clientes/clientes-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListClientesComponent } from './pages/list-clientes/list-clientes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', // Este es el camino por defecto del módulo
+    component: ListClientesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
