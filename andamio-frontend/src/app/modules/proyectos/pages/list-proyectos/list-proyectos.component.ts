fa54bf1194ade4f827244e412c52fd0ff9afdce1
Perro Maldito <88fritzz@gmail.com>
2026-05-06 07:39:15 -0600

    feat: estructura inicial del módulo de proyectos (modulo, componente y servicio)

---
+++ andamio-frontend/src/app/modules/proyectos/pages/list-proyectos/list-proyectos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-proyectos',
+  imports: [],
+  templateUrl: './list-proyectos.component.html',
+  styleUrl: './list-proyectos.component.scss'
+})
+export class ListProyectosComponent {
+
+}
