2a4b907c2429767bf546768ee7b8e052dec08881
Perro Maldito <88fritzz@gmail.com>
2026-05-05 21:16:17 -0600

    feat: initial angular structure and clientes module

---
+++ andamio-frontend/src/app/modules/clientes/pages/list-clientes/list-clientes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-clientes',
+  imports: [],
+  templateUrl: './list-clientes.component.html',
+  styleUrl: './list-clientes.component.scss'
+})
+export class ListClientesComponent {
+
+}
